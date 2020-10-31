8a162cb1f13b16954a407cc16a0b91b237d4cb87
Alison Williams <apw5011@gmail.com>
2020-10-31 19:11:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,10 +35,6 @@ def won?(board)
     position_7 = board[win_index_7]
     position_8 = board[win_index_8]
     
-    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
-      return win_combination
-    else
-      return false 
-    end 
+    position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
   end
 end
